4323f6e30e41896d80d3ca7caf7d6a131805450e
SATYAM <126665079+Raichuop07@users.noreply.github.com>
2023-06-16 23:08:39 +0530

    Add files via upload

---
+++ AnonX/plugins/active.py
@@ -0,0 +1,74 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from strings import get_command
+from AnonX import app
+from AnonX.misc import SUDOERS
+from AnonX.utils.database.memorydatabase import (
+    get_active_chats, get_active_video_chats)
+
+# Commands
+ACTIVEVC_COMMAND = get_command("ACTIVEVC_COMMAND")
+ACTIVEVIDEO_COMMAND = get_command("ACTIVEVIDEO_COMMAND")
+FAST_AC = get_command("FAST_AC")
+
+
+@app.on_message(filters.command(ACTIVEVC_COMMAND) & SUDOERS)
+async def activevc(_, message: Message):
+    mystic = await message.reply_text(
+        "ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇᴄʜᴀᴛs ʟɪsᴛ..."
+    )
+    served_chats = await get_active_chats()
+    text = ""
+    j = 0
+    for x in served_chats:
+        try:
+            title = (await app.get_chat(x)).title
+        except Exception:
+            title = "ᴩʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ"
+        if (await app.get_chat(x)).username:
+            user = (await app.get_chat(x)).username
+            text += f"<b>{j + 1}.</b>  [{title}](https://t.me/{user})[`{x}`]\n"
+        else:
+            text += f"<b>{j + 1}. {title}</b> [`{x}`]\n"
+        j += 1
+    if not text:
+        await mystic.edit_text("ɴᴏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇᴄʜᴀᴛs ᴏɴ ᴍᴜsɪᴄʙᴏᴛ...")
+    else:
+        await mystic.edit_text(
+            f"**ʟɪsᴛ ᴏғ ᴄᴜʀʀᴇɴᴛʟʏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇᴄʜᴀᴛs ᴏɴ ᴍᴜsɪᴄ ʙᴏᴛ :-**\n\n{text}",
+            disable_web_page_preview=True,
+        )
+
+
+@app.on_message(filters.command(ACTIVEVIDEO_COMMAND) & SUDOERS)
+async def activevi_(_, message: Message):
+    mystic = await message.reply_text(
+        "ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛs ʟɪsᴛ..."
+    )
+    served_chats = await get_active_video_chats()
+    text = ""
+    j = 0
+    for x in served_chats:
+        try:
+            title = (await app.get_chat(x)).title
+        except Exception:
+            title = "ᴩʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ"
+        if (await app.get_chat(x)).username:
+            user = (await app.get_chat(x)).username
+            text += f"<b>{j + 1}.</b>  [{title}](https://t.me/{user})[`{x}`]\n"
+        else:
+            text += f"<b>{j + 1}. {title}</b> [`{x}`]\n"
+        j += 1
+    if not text:
+        await mystic.edit_text("ɴᴏ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛs ᴏɴ ᴍᴜsɪᴄ ʙᴏᴛ...")
+    else:
+        await mystic.edit_text(
+            f"**ʟɪsᴛ ᴏғ ᴄᴜʀʀᴇɴᴛʟʏ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛs ᴏɴ ᴍᴜsɪᴄ ʙᴏᴛ :-**\n\n{text}",
+            disable_web_page_preview=True,
+        )
+@app.on_message(filters.command(FAST_AC) & SUDOERS)
+async def littleac(_, message: Message):
+    ac_audio = str(len(await get_active_chats()))
+    ac_video = str(len(await get_active_video_chats()))
+    await message.reply_text(f"𝗕𝗼𝘁 𝗔𝗰𝘁𝗶𝘃𝗲 𝗖𝗵𝗮𝘁𝘀 𝗜𝗻𝗳𝗼 • 🔊\n•━━━━━━━━━━━━━━━━━━•\n🎧 ᴀᴜᴅɪᴏ 🎧 » {ac_audio} Active\n•───────•\n🎥 ᴠɪᴅᴇᴏ 🎥 » {ac_video} Active\n•──────•", quote=True)
